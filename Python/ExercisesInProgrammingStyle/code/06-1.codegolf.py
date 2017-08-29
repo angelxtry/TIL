fc8c917552829a7bc638a79ab63158270b745df5
angelxtry <angelxtry@gmail.com>
2017-08-29 21:19:32 +0900

    python programming pattern 06-codegolf 추가

---
+++ Python/ExercisesInProgrammingStyle/code/06-1.codegolf.py
@@ -0,0 +1,6 @@
+import heapq, re, sys
+
+words = re.findall('[a-z]{2,}', open(sys.argv[1]).read().lower())
+for w in heapq.nlargest(25, set(words) - set(
+        open('../stop_words.txt').read().split(',')), words.count):
+    print(f'{w} - {words.count(w)}')

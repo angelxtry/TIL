27dd632e1d48c2ea33dd95044ac778889435dad8
LeeSuHo <angelxtry@gmail.com>
2019-06-05 22:45:16 +0900

    Update FJP

--- Javascirpt/FJP-2nd/fx.js
+++ Javascirpt/FJP-2nd/fx.js
@@ -1,22 +1,27 @@
 const log = console.log;
 
-const map = (f, iter) => {
+const add = (a, b) => a + b;
+
+const curry = f =>
+  (a, ..._) => _.length ? f(a, ..._) : (..._) => f(a, ..._);
+
+const map = curry((f, iter) => {
   let res = [];
   for (const p of iter) {
     res.push(f(p));
   }
   return res;
-}
+});
 
-const filter = (f, iter) => {
+const filter = curry((f, iter) => {
   let res = [];
   for (const p of iter) {
     if (f(p)) res.push(p);
   }
   return res;
-};
+});
 
-const reduce = (f, acc, iter) => {
+const reduce = curry((f, acc, iter) => {
   if (!iter) {
     iter = acc[Symbol.iterator]();
     acc = iter.next().value;
@@ -25,5 +30,8 @@ const reduce = (f, acc, iter) => {
     acc = f(acc, a);
   }
   return acc;
-}
+});
 
+const go = (...args) => reduce((a, f) => f(a), args);
+
+const pipe = (f, ...fs) => (...as) => go(f(...as), ...fs);
--- Javascirpt/FJP-2nd/script.js
+++ Javascirpt/FJP-2nd/script.js
@@ -33,5 +33,52 @@ const products = [
 //     0,
 //     products));
 
-log(map(p => p.price, products));
-log(map(p => p.price, filter(p => p.price < 20000, products)));
+
+go(
+  0,
+  a => a + 1,
+  a => a + 10,
+  a => a + 100,
+  log
+);
+
+// const pipe = (...fs) => (a) => go(a, ...fs);
+
+const f = pipe(
+  add,
+  a => a + 10,
+  a => a + 100);
+
+log(f(0, 2));
+
+go(
+  products,
+  products => filter(p => p.price < 20000, products),
+  products => map(p => p.price, products),
+  prices => reduce(add, prices),
+  log);
+
+go(
+  products,
+  products => filter(p => p.price < 20000)(products),
+  products => map(p => p.price)(products),
+  prices => reduce(add)(prices),
+  log);
+ 
+go(
+  products,
+  filter(p => p.price < 20000),
+  map(p => p.price),
+  reduce(add),
+  log);
+
+const mult = curry((a, b) => a * b);
+log(mult);
+log(mult(3));
+log(mult(3)(5));
+
+const mult3 = mult(3);
+log(mult3(20));
+log(mult3(10));
+log(mult3(5));
+

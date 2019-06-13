cb440ec0e84ab1397933867dc8991766489883c6
angelxtry <angelxtry@gmail.com>
2019-06-13 09:15:21 +0900

    Update FJP: L.flatten, L.deepFlat, yield *

--- Javascirpt/FJP-2nd/script.js
+++ Javascirpt/FJP-2nd/script.js
@@ -11,6 +11,44 @@ const sum = curry((f, iter) => go(
   map(f),
   reduce(add)));
 
-log(queryStr({ limit: 10, offset: 10, type: 'notice' }));
+// log(queryStr({ limit: 10, offset: 10, type: 'notice' }));
 
-log(filter(a => a % 2, range(10)));
+// log(filter(a => a % 2, range(10)));
+
+const isIterable = a => a && a[Symbol.iterator];
+
+// L.flatten = function *(iter) {
+//   for (const a of iter) {
+//     if (isIterable(a)) {
+//       for (const b of a) yield b;
+//     }
+//     else yield a;
+//   }
+// };
+
+L.flatten = function *(iter) {
+  for (const a of iter) {
+    if (isIterable(a)) yield *a;
+    else yield a;
+  }
+};
+
+var it = L.flatten([[1, 2], 3, 4, [5, 6], [7, 8, 9]]);
+log(take(5, it));
+
+const flatten = pipe(L.flatten, takeAll);
+log(flatten([[1, 2], 3, 4, [5, 6], [7, 8, 9]]));
+
+L.deepFlat = function *f(iter) {
+  for (const a of iter) {
+    if (isIterable(a)) yield *f(a);
+    else yield a;
+  }
+};
+
+var it = L.deepFlat([1, [2, [3, [4], [5, 6]]]]);
+log(...it);
+
+log([[1, 2], [3, 4], [5, 6, 7]].flatMap(a => a.map(a => a * a)));
+log(flatten([[1, 2], [3, 4], [5, 6, 7]].map(a => a.map(a => a * a))));
+
